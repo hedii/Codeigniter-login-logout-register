44c08d61804859645ce1bdb5b878cf34b4dc6767
hedii <hedi.chaibs@gmail.com>
2015-04-25 14:54:02 +0200

    setup register form

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -21,9 +21,27 @@ class User extends CI_Controller {
 	
 	public function register() {
 		
-		$this->load->view('header');
-		$this->load->view('user/register');
-		$this->load->view('footer');
+		// create the $data object
+		$data = new stdClass();
+		
+		// load form helper and validation library
+		$this->load->helper('form');
+		$this->load->library('form_validation');
+		
+		// set validation rules
+		$this->form_validation->set_rules('username', 'Username', 'trim|required|alpha_numeric');
+		$this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
+		$this->form_validation->set_rules('password', 'Password', 'trim|required');
+		$this->form_validation->set_rules('password_confirm', 'Password Confirm', 'trim|required');
+		
+		if ($this->form_validation->run() === false) {
+			
+			// validation not ok, send errors to the view
+			$this->load->view('header');
+			$this->load->view('user/register', $data);
+			$this->load->view('footer');
+			
+		}
 		
 	}
 		
--- application/views/user/register.php
+++ application/views/user/register.php
@@ -1,26 +1,35 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 <div class="container">
 	<div class="row">
+		<?php if (validation_errors()) : ?>
+			<div class="col-md-12">
+				<div class="alert alert-danger" role="alert">
+					<?= validation_errors() ?>
+				</div>
+			</div>
+		<?php endif; ?>
 		<div class="col-md-12">
-			<div class="form-group">
-				<label for="username">Username</label>
-				<input type="text" class="form-control" id="username" name="username" placeholder="Enter a username">
-			</div>
-			<div class="form-group">
-				<label for="email">Email</label>
-				<input type="email" class="form-control" id="email" name="email" placeholder="Enter your email">
-			</div>
-			<div class="form-group">
-				<label for="password">Password</label>
-				<input type="password" class="form-control" id="password" name="password" placeholder="Enter a password">
-			</div>
-			<div class="form-group">
-				<label for="password_confirm">Confirm password</label>
-				<input type="password" class="form-control" id="password_confirm" name="password_confirm" placeholder="Confirm your password">
-			</div>
-			<div class="form-group">
-				<input type="submit" class="btn btn-default" value="Register">
-			</div>
+			<?= form_open() ?>
+				<div class="form-group">
+					<label for="username">Username</label>
+					<input type="text" class="form-control" id="username" name="username" placeholder="Enter a username">
+				</div>
+				<div class="form-group">
+					<label for="email">Email</label>
+					<input type="email" class="form-control" id="email" name="email" placeholder="Enter your email">
+				</div>
+				<div class="form-group">
+					<label for="password">Password</label>
+					<input type="password" class="form-control" id="password" name="password" placeholder="Enter a password">
+				</div>
+				<div class="form-group">
+					<label for="password_confirm">Confirm password</label>
+					<input type="password" class="form-control" id="password_confirm" name="password_confirm" placeholder="Confirm your password">
+				</div>
+				<div class="form-group">
+					<input type="submit" class="btn btn-default" value="Register">
+				</div>
+			</form>
 		</div>
 	</div><!-- .row -->
 </div><!-- .container -->
