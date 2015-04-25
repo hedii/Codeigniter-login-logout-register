ad2daa246a8e581fb10349e16692fb2ba69f09b3
hedii <hedi.chaibs@gmail.com>
2015-04-25 12:33:08 +0200

    remove codeigniter index.php URL
    add htaccess, modify config file

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'http://codeigniter-forum.dev/';
 
 /*
 |--------------------------------------------------------------------------
@@ -29,7 +29,7 @@ $config['base_url'] = '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
