89a0e9325466f8cebf5ff8427af8f0c9abe149dc
hedii <hedi.chaibs@gmail.com>
2015-04-25 16:40:29 +0200

    add help block below registration form fields

--- application/views/user/register/register.php
+++ application/views/user/register/register.php
@@ -16,22 +16,29 @@
 			</div>
 		<?php endif; ?>
 		<div class="col-md-12">
+			<div class="page-header">
+				<h1>Register</h1>
+			</div>
 			<?= form_open() ?>
 				<div class="form-group">
 					<label for="username">Username</label>
 					<input type="text" class="form-control" id="username" name="username" placeholder="Enter a username">
+					<p class="help-block">At least 4 characters, letters or numbers only</p>
 				</div>
 				<div class="form-group">
 					<label for="email">Email</label>
 					<input type="email" class="form-control" id="email" name="email" placeholder="Enter your email">
+					<p class="help-block">A valid email address</p>
 				</div>
 				<div class="form-group">
 					<label for="password">Password</label>
 					<input type="password" class="form-control" id="password" name="password" placeholder="Enter a password">
+					<p class="help-block">At least 6 characters</p>
 				</div>
 				<div class="form-group">
 					<label for="password_confirm">Confirm password</label>
 					<input type="password" class="form-control" id="password_confirm" name="password_confirm" placeholder="Confirm your password">
+					<p class="help-block">Must match your password</p>
 				</div>
 				<div class="form-group">
 					<input type="submit" class="btn btn-default" value="Register">
